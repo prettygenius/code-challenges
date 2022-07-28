1c8a21394316319df9b2c40056f2a2d1427ab76b
prettygenius <devprettygenius@gmail.com>
2022-07-27 21:10:33 -0500

    new code challenge for the day

---
+++ codejuly22/find-smallest-int-in-array.js
@@ -0,0 +1,13 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+      
+    }
+  }
