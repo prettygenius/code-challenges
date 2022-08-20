32db67af8549e69369cfdd0e8e2bf6d264682fea
prettygenius <devprettygenius@gmail.com>
2022-08-19 21:33:53 -0500

    new code challenge

---
+++ codeAug22/whats-the-real-floor.js
@@ -0,0 +1,21 @@
+// Write a function that given a floor in the american system returns the floor in the european system.
+
+// With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers move down to take their place. In case of above 13, they move down by two because there are two omitted numbers below them.
+
+// Basements (negatives) stay the same as the universal level.
+
+
+
+function getRealFloor(n) {
+    if(n >= 13) {
+      return n - 2
+    } else if (n > 0) {
+      return n - 1
+    } else {
+      return n
+    }
+      }
+    
+    
+    //all numbers less than thirteen but greater than 0 subtract 1, 
+    //subtract extra -1 greater than 13 since its removed
